1e29a7f46745c9dc272ae65546d4dd7e6802aeb4
gregm316 <gregm316@gmail.com>
2022-06-08 23:30:24 -0400

    Added questions and functions to index.js and added functions to generatemarkdown

--- index.js
+++ index.js
@@ -1,14 +1,77 @@
-const questions = [];
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a description of your project'
+    },
+    {
+        type: 'input',
+        name: 'tableofcontents',
+        message: 'Please write a description of your project'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the steps to install your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and examples for use.',
+    },
+    {
+        type: 'input',
+        name: 'tests', 
+        message: 'Please provide instructions and examples on how to run tests.',
+        default: 'npm test'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide guidelines for contributing.',
+    }
+];
 
 
 function writeToFile(fileName, data) {
-
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 }
 
 
 function init() {
-
-}
+inquirer
+  .prompt(questions)
+  .then((inquirerResponses) => {
+   writeToFile("README.md", generateMarkdown({ ...inquirerResponses }))
+  })
+};
 
 
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,15 +1,68 @@
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'no license') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    } else{
+      return '';
+    }
+  } 
 
 
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'no license') {
+    return `[${license}](https://choosealicense.com/licenses/${license})`;
+    } else {
+      return ' ';
+    }
+}
 
 
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'no license') {
+    return `
+    ## [License](#table-of-contents)
+    The application is covered under the following license:
+    ${renderLicenseLink(license)}
+      `;
+    } else {
+      return ' ';
+    }
+}
 
 function generateMarkdown(data) {
 
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table-of-Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseTOC(data.license)}
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## [Description](#table-of-contents) 
+  ${data.description}
+
+  ## [Installation](#table-of-contents) 
+  ${data.install}
+  
+  ## [Usage](#table-of-contents) 
+  ${data.usage}
+
+  ## [Contributing](#table-of-contents)
+  ${data.contributing}
+
+  ## [Tests](#table-of-contents)
+  ${data.test}
+
+  ## [Questions](#table-of-contents)
+
+  If you have any questions about this project, please contact me using the following links:
+  [GitHub](https://github.com/${data.github})
+  [Email: ${data.email}](mailto:${data.email})
 `;
 }
 
